042daeb7e079082d2e40dc8d59c7562da23001d6
pntech20 <pntech20@gmail.com>
2020-04-12 08:14:37 +0700

    Adding components from ReactiveSearch and styling them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  ReactiveBase,
+  DataSearch,
+  NumberBox,
+  DateRange,
+  RangeSlider
+} from "@appbaseio/reactivesearch";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <ReactiveBase
+          app="airbeds-test-app"
+          credentials="X8RsOu0Lp:9b4fe1a4-58c6-4089-a042-505d86d9da30"
+          type="listing"
+          theme={{
+            colors: {
+              primaryColor: "#41ABF5"
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="nav-container">
+            <nav className="nav">
+              <div className="title">Airbeds</div>
+            </nav>
+          </div>
+          <div className="filters-search-container">
+            <div className="filter-container">
+              <div className="dropdown">
+                <button className="button">Price</button>
+                <div className="dropdown-content">
+                  <RangeSlider
+                    componentId="PriceSensor"
+                    dataField="price"
+                    title="Price Range"
+                    range={{
+                      start: 10,
+                      end: 250
+                    }}
+                    rangeLabels={{
+                      start: "$10",
+                      end: "$250"
+                    }}
+                    defaultValue={{
+                      start: 10,
+                      end: 50
+                    }}
+                    stepValue={10}
+                    interval={20}
+                    react={{
+                      and: ["DateRangeSensor", "GuestSensor"]
+                    }}
+                    className="rangeFilter"
+                  />
+                </div>
+              </div>
+              <div className="dropdown">
+                <button className="button">Guests</button>
+                <div className="dropdown-content-guest">
+                  <NumberBox
+                    componentId="GuestSensor"
+                    dataField="accommodates"
+                    title="Guests"
+                    defaultValue={2}
+                    labelPosition="right"
+                    data={{
+                      start: 1,
+                      end: 16
+                    }}
+                    className="numberFilter"
+                  />
+                </div>
+              </div>
+
+              <div className="dropdown">
+                <button className="button ">When</button>
+                <div className="dropdown-content">
+                  <DateRange
+                    dataField="date_from"
+                    componentId="DateRangeSensor"
+                    title="When"
+                    numberOfMonths={2}
+                    queryFormat="basic_date"
+                    initialMonth={new Date("04/01/2017")}
+                    className="dateFilter"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="search-container">
+              <DataSearch
+                componentId="search"
+                dataField="name"
+                autosuggest={false}
+                placeholder="Search housings..."
+                iconPosition="left"
+                className="search"
+              />
+            </div>
+          </div>
+        </ReactiveBase>
+      </div>
+    );
+  }
 }
 
 export default App;
